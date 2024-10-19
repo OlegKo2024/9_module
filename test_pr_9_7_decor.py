08e4aba6947cccd219bba0cad929852b3947f5a1
OlegKo2024 <4oki66@gmail.com>
2024-10-19 14:14:57 +0300

    Add files via upload

---
+++ test_pr_9_7_decor.py
@@ -0,0 +1,75 @@
+print('Декораторы в Python')
+
+# Напишите 2 функции:
+# Функция, которая складывает 3 числа (sum_three)
+# Функция декоратор (is_prime), которая распечатывает "Простое", если результат 1ой функции будет простым числом и "Составное" в противном случае.
+# Пример:
+# result = sum_three(2, 3, 6)
+# print(result)
+
+print('Декораторы в Python')
+
+# Напишите 2 функции:
+# Функция, которая складывает 3 числа (sum_three)
+# Функция декоратор (is_prime), которая распечатывает "Простое", если результат 1ой функции будет простым числом и "Составное" в противном случае.
+# Пример:
+# result = sum_three(2, 3, 6)
+# print(result)
+
+print('Вариант 1')
+def is_prime(fun_time):
+    def checking_numbers(*args):
+        result = fun_time(*args)
+        is_prime = True
+        if result < 2:
+            is_prime = False
+        from math import sqrt
+        for i in range(2, int(sqrt(result)) + 1):
+            if result % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            print(f'{result} - Простое')
+        else:
+            print(f'{result} - Составное')
+        return result
+    return checking_numbers
+
+@is_prime
+def sum_them_all(*args):
+    return sum(args)
+
+
+result = sum_them_all(2, 3, 6, 1)
+print(result)
+
+
+print('Вариант 2')
+def is_prime(fun_time):
+    def checking_numbers(*args):
+        result = fun_time(*args)  # Получаем результат от функции
+        if result < 0:
+            print(f'{result} - Отрицательное число не является натуральным числом')
+            return result  # Выход из функции
+        if result < 2:
+            print(f'{result} - Не простое и не составное')  # Числа меньше 2 - составные
+            return result  # Выход из функции
+        is_prime = True  # Предполагаем, что результат - простое число
+        for i in range(2, int(result ** 0.5) + 1):  # Проверяем делители
+            if result % i == 0:  # Если нашли делитель
+                is_prime = False  # Число составное
+                break  # Прерываем цикл, так как нет смысла продолжать
+        if is_prime:
+            print(f'{result} - Простое')
+        else:
+            print(f'{result} - Составное')
+        return result
+    return checking_numbers
+
+@is_prime
+def sum_them_all(*args):
+    return sum(args)
+
+
+result = sum_them_all(2, 3, 6, 1)
+print(result)
